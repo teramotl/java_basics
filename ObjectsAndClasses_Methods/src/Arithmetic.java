047486cd3567945036e8f2afbdd327ff9728f72d
Behrozbek Togayev <teramotl@icloud.com>
2023-10-17 19:02:23 +0500

    arithmetics done

--- ObjectsAndClasses_Methods/src/Arithmetic.java
+++ ObjectsAndClasses_Methods/src/Arithmetic.java
@@ -1,3 +1,22 @@
 public class Arithmetic {
+    public int num1;
+    public int num2;
 
+    public Arithmetic(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int summary() {
+        return num1 + num2;
+    }
+    public int multiplication() {
+        return num1 * num2;
+    }
+    public int minNumber() {
+        return Math.min(num1, num2);
+    }
+    public int maxNumber() {
+        return Math.max(num1, num2);
+    }
 }
