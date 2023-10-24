8419d9cda1f7d9799d3382a06e940adad8a8f62b
Behrozbek Togayev <teramotl@icloud.com>
2023-10-24 22:41:16 +0500

    homework primitivi done

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -4,9 +4,9 @@ public class Main {
         container.addCount(5672);
         System.out.println(container.getCount());
 
-        // TODO: ниже напишите код для выполнения задания:
-        //  С помощью цикла и преобразования чисел в символы найдите все коды
-        //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
-
+        for (int i = 1040; i < 1105; i++) {
+            char c = (char) i;
+            System.out.println(i + " - " + c);
+        }
     }
 }
